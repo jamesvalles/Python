e79104043ac1b6dbce872be18b24d9ea15714063
James Valles <jamesvalles1@hotmail.com>
2017-04-03 18:32:05 -0500

    Add files via upload

---
+++ spelling.py
@@ -0,0 +1,8 @@
+#basic for loop in Python for CSC 100
+
+name = input('Enter your full name: ')
+print('Cool! Your name is spelled like this: ')
+
+#the following code causes the letters to print in iteration
+for char in name:
+    print(char)
